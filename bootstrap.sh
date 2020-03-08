268aec59a3d90933e42bdb1de3ec2cd0f0d0870b
Arun Kumar <arsakthi@cisco.com>
2020-03-07 23:43:16 -0800

    updated after testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,36 +2,33 @@
 
 echo "*******Running Automation Team Desktop Creation Bash Script*********"
 sudo apt-get update
+echo ""
 
 echo "******Installing PIP package*******"
 yes Y | sudo apt install python-pip
+echo ""
 
 echo "******** Installing Git*******"
 sudo apt install git
 sudo apt-get install -y sshpass
+echo ""
 
 echo "******Installing Ansible*******"
 echo "Installing Anisble Latest and Greatest"
 sudo apt update
 pip install ansible
-#
-#if [ -z "$1" ]
-#    then
-#        echo "Installing Anisble Latest and Greatest"
-#        sudo apt update
-#        yes Y | sudo apt install software-properties-common
-#        yes Y | sudo apt-add-repository --yes --update ppa:ansible/ansible
-#        yes Y | sudo apt install ansible
+echo ""
 
-#else
-        echo "Installing Anisble version $2"
-#        sudo pip install ansible==$2
-
-#fi
+# Clone github Repo
+echo "********Cloning Devbox setup Repo*******"
+git clone https://github.com/arsakthi/IDDevBox.git
+git pull
+echo ""
 
 # Run Playbook
-echo "Running DevBox Setup Playbook"
-ansible-playbook devbox.yml
+echo "******Running DevBox Setup Playbook******"
+cd IDDevBox
+ansible-playbook -i hosts devbox.yml
 echo ""
 
 # Done
